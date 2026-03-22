61b89b844f63436d8cc878627ea0c013134d91c3
JussiPennanen1 <jupe100@gmail.com>
2026-03-22 15:28:20 +0200

    Revert backend index.js to assignment version

--- index.js
+++ index.js
@@ -4,19 +4,6 @@ const cors = require('cors')
 const { todoRouter } = require('./routes/todo.js')
 
 const app = express()
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(204)
-  }
-
-  next()
-})
-
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
